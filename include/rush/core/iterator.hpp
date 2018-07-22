9fcb0bbddb036bf0a99c74557acb4e841f95c6ee
Reegan Layzell <reegan.layzell@live.co.uk>
2018-07-22 13:12:53 +0100

    Add expr SFINAE

--- include/rush/core/iterator.hpp
+++ include/rush/core/iterator.hpp
@@ -6,11 +6,15 @@
 
 namespace rush {
 	template <typename InIter, typename Pred>
-	auto advance_if(InIter& first, InIter const& last, Pred predicate) {
-		auto start = first;
-		while (first != last && predicate(*first)) ++first;
-		return start != first;
+	auto advance_if(
+		InIter& it,
+		InIter const& last,
+		Pred predicate) -> decltype(predicate(*it), bool()) {
+			auto first = it;
+			while (it != last && predicate(*it)) ++it;
+			return it != first;
 	}
+
 } // rush
 
 #endif // RUSH_CORE_ITERATOR_HPP
