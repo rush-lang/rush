cb5138c42cc0d305727b93bfa9b29a2b3487a54b
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 00:39:36 +0100

    Make constructor and virtual destructor public

--- include/rush/ast/node.hpp
+++ include/rush/ast/node.hpp
@@ -6,14 +6,20 @@
 namespace rush::ast {
 	class visitor;
 
+	// enum class statement_type : std::uint8_t;
+	// enum class expression_type : std::uint8_t;
+	// enum class declaration_type : std::uint8_t;
+
+
 	class node {
 	protected:
-		node() = default;
-		virtual ~node() = default;
 		node(node const&) = delete;
 		void operator = (node const&) = delete;
 
 	public:
+		node() = default;
+		virtual ~node() = default;
+
 		virtual void accept(ast::visitor&) const = 0;
 	};
 } // rush
