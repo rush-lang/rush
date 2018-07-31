31c6ee494004deac52b17904bcf824c220497052
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 00:41:37 +0100

    Stub built-in types

--- include/rush/ast/type.hpp
+++ include/rush/ast/type.hpp
@@ -13,6 +13,22 @@ namespace rush::ast {
 
 		virtual void accept(ast::visitor&) const;
 	};
+
+	// known/built-in types
+	extern type const& void_type;
+	extern type const& bool_type;
+	extern type const& byte_type;
+	extern type const& sbyte_type;
+	extern type const& short_type;
+	extern type const& ushort_type;
+	extern type const& int_type;
+	extern type const& uint_type;
+	extern type const& long_type;
+	extern type const& ulong_type;
+	extern type const& float_type;
+	extern type const& double_type;
+	extern type const& string_type;
+	extern type const& char_type;
 }
 
 #endif // RUSH_AST_TYPE_HPP
