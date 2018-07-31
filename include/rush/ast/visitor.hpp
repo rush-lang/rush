2fc6d417d80217e61b347673768427dd2b6e691d
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 00:42:29 +0100

    Stub visitor base classes

--- include/rush/ast/visitor.hpp
+++ include/rush/ast/visitor.hpp
@@ -4,9 +4,34 @@
 #define RUSH_AST_VISITOR_HPP
 
 namespace rush::ast {
+	class expression;
+	class unary_expression;
+	class binary_expression;
+	class literal_expression;
+	class variable_expression;
 
 	class visitor {
+	public:
+		virtual ~visitor() = default;
 
+		virtual void unary_expr(unary_expression const&) = 0;
+		virtual void binary_expr(binary_expression const&) = 0;
+		virtual void literal_expr(literal_expression const&) = 0;
+		virtual void variable_expr(variable_expression const&) = 0;
+	};
+
+	// todo: generate this via injen instead.
+	class descending_visitor : private visitor {
+		virtual void unary_expr(unary_expression const&) override;
+		virtual void binary_expr(binary_expression const&) override;
+		virtual void literal_expr(literal_expression const&) override;
+		virtual void variable_expr(variable_expression const&) override;
+
+	public:
+		virtual void addition_expr(binary_expression const&);
+		virtual void subtraction_expr(binary_expression const&);
+		virtual void multiplication_expr(binary_expression const&);
+		virtual void division_expr(binary_expression const&);
 	};
 } // rush
 
