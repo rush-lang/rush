9aff6608d59e1faec27ac7d51a8fa176fdae241e
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 00:38:27 +0100

    Use unique_ptrs to start with and add binary_operator enum

--- include/rush/ast/binary.hpp
+++ include/rush/ast/binary.hpp
@@ -1,46 +1,50 @@
 #pragma once
 
-#ifndef RUSH_AST_BINARY_EXPRESSION
-#define RUSH_AST_BINARY_EXPRESSION
+#ifndef RUSH_AST_BINARY_HPP
+#define RUSH_AST_BINARY_HPP
 
 #include "rush/ast/expression.hpp"
 
 #include <memory>
 
 namespace rush::ast {
+	enum class binary_operator {
+		addition,
+		subtraction,
+		multiplication,
+		division,
+		modulo
+	};
+
 	class binary_expression : public expression {
+		friend std::unique_ptr<binary_operator> make_binary_expr(
+			std::unique_ptr<expression>,
+			std::unique_ptr<expression>,
+			binary_operator);
+
 	public:
-		binary_expression(std::unique_ptr<expression> left, std::unique_ptr<expression> right)
-			: _left(this->allocate(std::move(left)))
-			, _right(this->allocate(std::move(right))) {}
-
-		binary_expression(binary_expression&& other)
-			: _left(std::move(other._left))
-			, _right(std::move(other._right)) {}
-
-		binary_expression& operator = (binary_expression&& other) {
-		}
-
+		binary_operator opkind() const noexcept { return _opkind; }
 		expression const& left() const noexcept { return *_left; }
 		expression const& right() const noexcept { return *_right; }
 
-		// virtual expression clone() const override {
-		// 	return binary_expression { _left->clone(), _right->clone() };
-		// }
+		virtual void accept(ast::visitor&) const override;
 
-		virtual void accept(ast::visitor&) const override {
-
-		}
 	private:
-		expression* _left;
-		expression* _right;
-	};
+		std::unique_ptr<expression> _left;
+		std::unique_ptr<expression> _right;
+		binary_operator _opkind;
 
+		binary_expression(std::unique_ptr<expression> left, std::unique_ptr<expression> right, binary_operator op)
+			: _left(std::move(left))
+			, _right(std::move(right))
+			, _opkind(op) {}
+	};
 
 	std::unique_ptr<binary_expression> addition_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
 	std::unique_ptr<binary_expression> subtraction_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
 	std::unique_ptr<binary_expression> multiplication_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
 	std::unique_ptr<binary_expression> division_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
+	std::unique_ptr<binary_expression> modulo_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
 } // rush
 
-#endif // RUSH_AST_BINARY_EXPRESSION
+#endif // RUSH_AST_BINARY_HPP
