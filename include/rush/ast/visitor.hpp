d61c0869bb0acae1c0991c76cfa34ab6bae166df
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 13:48:19 +0100

    Rename visit member functions and add visitation member function for ast::type

--- include/rush/ast/visitor.hpp
+++ include/rush/ast/visitor.hpp
@@ -4,6 +4,7 @@
 #define RUSH_AST_VISITOR_HPP
 
 namespace rush::ast {
+	class type;
 	class expression;
 	class unary_expression;
 	class binary_expression;
@@ -14,24 +15,26 @@ namespace rush::ast {
 	public:
 		virtual ~visitor() = default;
 
-		virtual void unary_expr(unary_expression const&) = 0;
-		virtual void binary_expr(binary_expression const&) = 0;
-		virtual void literal_expr(literal_expression const&) = 0;
-		virtual void variable_expr(variable_expression const&) = 0;
+		virtual void visit_type(ast::type const&) = 0;
+		virtual void visit_unary_expr(unary_expression const&) = 0;
+		virtual void visit_binary_expr(binary_expression const&) = 0;
+		virtual void visit_literal_expr(literal_expression const&) = 0;
+		virtual void visit_variable_expr(variable_expression const&) = 0;
 	};
 
 	// todo: generate this via injen instead.
 	class descending_visitor : private visitor {
-		virtual void unary_expr(unary_expression const&) override;
-		virtual void binary_expr(binary_expression const&) override;
-		virtual void literal_expr(literal_expression const&) override;
-		virtual void variable_expr(variable_expression const&) override;
+		virtual void visit_type(ast::type const&) override;
+		virtual void visit_unary_expr(unary_expression const&) override;
+		virtual void visit_binary_expr(binary_expression const&) override;
+		virtual void visit_literal_expr(literal_expression const&) override;
+		virtual void visit_variable_expr(variable_expression const&) override;
 
 	public:
-		virtual void addition_expr(binary_expression const&);
-		virtual void subtraction_expr(binary_expression const&);
-		virtual void multiplication_expr(binary_expression const&);
-		virtual void division_expr(binary_expression const&);
+		virtual void visit_addition(binary_expression const&);
+		virtual void visit_subtraction(binary_expression const&);
+		virtual void visit_multiplication(binary_expression const&);
+		virtual void visit_division(binary_expression const&);
 	};
 } // rush
 
