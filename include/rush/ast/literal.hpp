e0ce12aa2b50a970c0ae676236658ca53e557f8d
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 13:46:29 +0100

    Add factory functions for literal expressions

--- include/rush/ast/literal.hpp
+++ include/rush/ast/literal.hpp
@@ -11,23 +11,54 @@
 
 namespace rush::ast {
 	class literal_expression : public expression {
-		friend std::unique_ptr<literal_expression> literal_expr(std::string);
-		friend std::unique_ptr<literal_expression> literal_expr(std::uint64_t, type const&);
+		struct factory_tag_t {};
 
 		using variant_type = std::variant<
 			std::string,
 			std::uint64_t,
-			double>;
-	public:
+			double, bool>;
 
-		virtual void accept(ast::visitor&) const override;
+		friend std::unique_ptr<literal_expression> make_literal_expr(
+			variant_type val, type const& val_type);
+
+	public:
+		literal_expression(variant_type val, ast::type const& val_type, factory_tag_t)
+			: _val(std::move(val))
+			, _type(val_type) {}
+
+		virtual type result_type() const noexcept override {
+			return _type;
+		}
+
+		virtual void accept(ast::visitor& vis) const override {
+			vis.visit_literal_expr(*this);
+		}
+
 	private:
 		variant_type _val;
-		literal_expression(variant_type val) : _val(std::move(val)) {}
+		type _type;
+
+		friend std::unique_ptr<literal_expression> make_literal_expr(
+			variant_type val,
+			ast::type const& val_type);
 	};
 
+	std::unique_ptr<literal_expression> literal_expr(bool);
 	std::unique_ptr<literal_expression> literal_expr(std::string);
-	std::unique_ptr<literal_expression> literal_expr(std::uint64_t, type const&);
+
+	std::unique_ptr<literal_expression> literal_expr(std::int8_t, ast::type const& = sbyte_type);
+	std::unique_ptr<literal_expression> literal_expr(std::int16_t, ast::type const& = short_type);
+	std::unique_ptr<literal_expression> literal_expr(std::int32_t, ast::type const& = int_type);
+	std::unique_ptr<literal_expression> literal_expr(std::int64_t, ast::type const& = long_type);
+
+	std::unique_ptr<literal_expression> literal_expr(std::uint8_t, ast::type const& = byte_type);
+	std::unique_ptr<literal_expression> literal_expr(std::uint16_t, ast::type const& = ushort_type);
+	std::unique_ptr<literal_expression> literal_expr(std::uint32_t, ast::type const& = uint_type);
+	std::unique_ptr<literal_expression> literal_expr(std::uint64_t, ast::type const& = uint_type);
+
+	std::unique_ptr<literal_expression> literal_expr(float, ast::type const& = float_type);
+	std::unique_ptr<literal_expression> literal_expr(double, ast::type const& = double_type);
+	std::unique_ptr<literal_expression> literal_expr(long double, ast::type const& = double_type);
 } // rush
 
 #endif // RUSH_AST_LITERAL_HPP
--- lib/ast/literal.cpp
+++ lib/ast/literal.cpp
@@ -1,7 +1,65 @@
 #include "rush/ast/literal.hpp"
 
 namespace rush::ast {
-	void literal_expression::accept(ast::visitor&) const {
+	inline std::unique_ptr<literal_expression> make_literal_expr(
+		literal_expression::variant_type val,
+		ast::type const& val_type
+	) {
+		return std::make_unique<literal_expression>(
+			std::move(val), val_type,
+			literal_expression::factory_tag_t{}
+		);
+	}
 
+	std::unique_ptr<literal_expression> literal_expr(bool val) {
+		return make_literal_expr(val, ast::bool_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::string val) {
+		return make_literal_expr(val, ast::string_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::int8_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::int16_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::int32_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::int64_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::uint8_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::uint16_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::uint32_t val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<std::uint64_t>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(std::uint64_t val, ast::type const& val_type) {
+		return make_literal_expr(val, val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(float val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<double>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(double val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<double>(val), val_type);
+	}
+
+	std::unique_ptr<literal_expression> literal_expr(long double val, ast::type const& val_type) {
+		return make_literal_expr(static_cast<double>(val), val_type);
 	}
 }
