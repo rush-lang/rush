3632399650308df4df06a170e656fae71a63ba14
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-02 00:59:00 +0100

    Begin implementation of parser with literal expressions

--- lib/parser/parser.cpp
+++ lib/parser/parser.cpp
@@ -1,6 +1,14 @@
+#include "rush/core/iterator.hpp"
+
 #include "rush/lexer/token.hpp"
 #include "rush/lexer/lexer.hpp"
 #include "rush/lexer/analysis.hpp"
+
+#include "rush/ast/type.hpp"
+#include "rush/ast/expr/unary.hpp"
+#include "rush/ast/expr/binary.hpp"
+#include "rush/ast/expr/literal.hpp"
+#include "rush/ast/expr/identifier.hpp"
 
 #include "rush/parser/parser.hpp"
 
@@ -11,15 +19,16 @@
 
 
 using namespace rush;
-
-namespace tok = rush::tokens;
-namespace sym = rush::symbols;
-namespace kws = rush::keywords;
+namespace sy = rush::symbols;
+namespace kw = rush::keywords;
 
 using ltt = rush::lexical_token_type;
 
 
 class parser {
+	using lxa_iterator = lexical_analysis::const_iterator;
+	using lxa_iterator_difference_type = std::iterator_traits<lxa_iterator>::difference_type;
+
 public:
 	parser(parser_options const& opts)
 		: _options(opts) {}
@@ -31,10 +40,12 @@ public:
 	}
 
 private:
+	static const lexical_token eof;
+
 	parser_options _options;
 	std::pair<
-		lexical_analysis::const_iterator,
-		lexical_analysis::const_iterator> _range;
+		lxa_iterator,
+		lxa_iterator> _range;
 
 	void initialize(lexical_analysis const& lxa) {
 		_range = {
@@ -43,43 +54,127 @@ private:
 		};
 	}
 
-	lexical_token const& peek() {
-		return *_range.first;
-	}
-
 	template <typename... Args>
 	void fatal(std::string msg, Args&&... args) {
-
 	}
 
 	template <typename... Args>
 	void error(std::string msg, Args&&... args) {
-
 	}
 
 	template <typename... Args>
 	void warn(std::string msg, Args&&... args) {
-
 	}
 
-	template <typename... Args>
-	void info(std::string msg, Args&&... args) {
+	lexical_token const& peek(lxa_iterator_difference_type offset = 0) {
+		auto temp = _range.first;
+		advance(temp, _range.second, offset);
+		return temp != _range.second ? *temp : eof;
+	}
 
+	lexical_token const& peek_skip_indent(lxa_iterator_difference_type offset = 0) {
+		auto* ptok = &peek(offset);
+		for (decltype(offset) i = offset; ptok->is_any(sy::indent, sy::dedent); ptok = &peek(i + offset), ++i);
+		return *ptok;
+	}
+
+	lexical_token const& next_skip_indent() {
+		auto* ptok = &next();
+		for (; ptok->is_any(sy::indent, sy::dedent); ptok = &next());
+		return *ptok;
+	}
+
+	lexical_token const& next() {
+		if (advance(_range.first, _range.second, 1)) return *_range.first;
+		return eof;
+	}
+
+	ast::type const& parse_type() {
+		auto& curtok = peek_skip_indent();
+		if (curtok.is_keyword()) {
+			switch (curtok.keyword()) {
+			case kw::void_: return ast::void_type;
+			case kw::bool_: return ast::bool_type;
+			case kw::sbyte_: return ast::sbyte_type;
+			case kw::byte_: return ast::byte_type;
+			case kw::int_: return ast::int_type;
+			case kw::uint_: return ast::uint_type;
+			case kw::long_: return ast::long_type;
+			case kw::ulong_: return ast::ulong_type;
+			case kw::float_: return ast::float_type;
+			case kw::double_: return ast::double_type;
+			case kw::string_: return ast::string_type;
+			default: break;
+			};
+		}
+		else if (curtok.is_identifier()) {
+			// todo: find entry in the symbol table, if it doesn't
+			// exist, no matter, the entry may be filled later
+			// in the program.
+		}
+
+		error("expected a type, but found '{}'", curtok);
+		return ast::none_type;
 	}
 
 	std::unique_ptr<ast::literal_expression> parse_literal_expression() {
+		auto& curtok = peek_skip_indent();
+
+		if (curtok.is(kw::true_)) return ast::literal_expr(true);
+		if (curtok.is(kw::false_)) return ast::literal_expr(false);
+		if (curtok.is_string_literal()) return ast::literal_expr(curtok.text());
+
+		if (curtok.is_integer_literal()) {
+			switch (curtok.suffix()) {
+			case lexical_token_suffix::none: return ast::literal_expr(curtok.integer_value(), ast::int_type);
+			case lexical_token_suffix::long_literal: return ast::literal_expr(curtok.integer_value(), ast::long_type);
+			case lexical_token_suffix::unsigned_literal: return ast::literal_expr(curtok.integer_value(), ast::uint_type);
+			// case lexical_token_suffix::unsigned_long_literal: return ast::literal_expr(curtok.integer(), ast::ulong_type);
+			default: throw;
+			}
+		}
+
+		if (curtok.is_floating_literal()) {
+			switch (curtok.suffix()) {
+			case lexical_token_suffix::none: return ast::literal_expr(curtok.floating_value(), ast::double_type);
+			case lexical_token_suffix::float_literal: return ast::literal_expr(curtok.floating_value(), ast::float_type);
+			default: throw;
+			}
+		}
+
+		error("expected literal expression but found '{}'", curtok);
 		return nullptr;
 	}
 
 	std::unique_ptr<ast::binary_expression> parse_binary_expression() {
+		error("expected binary expression but found '{}'", nullptr);
 		return nullptr;
 	}
 
 	std::unique_ptr<ast::unary_expression> parse_unary_expression() {
+		error("expected unary expression but found '{}'", nullptr);
 		return nullptr;
+	}
+
+	std::unique_ptr<ast::expression> parse_primary_expression() {
+		auto curtok = peek();
+		switch (curtok.type()) {
+		default:
+			error("unknown token '{}' when parsing expression.", curtok);
+		case lexical_token_type::error:
+			error(curtok.text());
+			return nullptr;
+		case lexical_token_type::identifier:
+			return nullptr;
+		case lexical_token_type::string_literal:
+		case lexical_token_type::integer_literal:
+		case lexical_token_type::floating_literal:
+			return parse_literal_expression();
+		}
 	}
 };
 
+const lexical_token parser::eof = tokens::eof();
 
 namespace rush {
 
--- test/unit-tests/parser/parser_test.cpp
+++ test/unit-tests/parser/parser_test.cpp
@@ -1,8 +1,16 @@
 #include "catch2/catch.hpp"
 #include "rush/parser/parser.hpp"
 
-TEST_CASE("rush::parse", "[unit][parser]") {
-	// auto ast = rush::parse("main() => 0");
-	auto ast = rush::parse("main() => 0");
+#include <string>
 
+namespace ast = rush::ast;
+
+bool valid_parse(std::string input, std::unique_ptr<ast::node> root) {
+	return false;
+}
+
+TEST_CASE("rush::parse", "[unit][parser]") {
+	// CHECK( valid_parse("let i = 0", ast::constant_decl("i", ast::int_type, ast::literal_expr(0))));
+	// CHECK( valid_parse("let d = 0.1", ast::constant_decl("d", ast::double_type, ast::literal_expr(0))));
+	// CHECK( valid_parse("let s = \"abc\"", ast::constant_decl("s", ast::string_type, ast::literal_expr(0))));
 }
