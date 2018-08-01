7566c85d67b31309f6e5ee4ce5c537bf442f976e
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 19:26:23 +0100

    Add tests for lexical_token::text()

--- test/unit-tests/lexer/token_test.cpp
+++ test/unit-tests/lexer/token_test.cpp
@@ -1,6 +1,8 @@
 #include "catch2/catch.hpp"
 #include "rush/lexer/token.hpp"
 
+#include <iostream>
+
 using namespace rush::tokens;
 namespace symbols = rush::symbols;
 namespace keywords = rush::keywords;
@@ -15,6 +17,17 @@ TEST_CASE( "rush::lexical_token", "[unit][lexer]" ) {
 		CHECK( if_keyword().is(keywords::if_) );
 		CHECK( else_keyword().is(keywords::else_) );
 		CHECK( while_keyword().is(keywords::while_) );
+	}
+
+	SECTION( "rush::lexical_token::text" ) {
+		CHECK( identifier("abc").text() == "abc" );
+		CHECK( void_keyword().text() == "void" );
+		CHECK( integer_literal(123).text() == "123" );
+
+		// todo: should probably do something about the formatting of
+		// decimals. not that important though as lexical_token::text()
+		// is not expected to be used outside of diagnostics.
+		CHECK( floating_literal(123.321).text() == "123.321000" );
 	}
 
 	SECTION( "rush::lexical_token::is_any" ) {
