57a93dc96f9c083c66d5093c5500cdd6d85292d8
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-01 13:49:55 +0100

    Add factory functions binary expressions

--- include/rush/ast/binary.hpp
+++ include/rush/ast/binary.hpp
@@ -8,7 +8,7 @@
 #include <memory>
 
 namespace rush::ast {
-	enum class binary_operator {
+	enum class binary_operator : std::uint8_t {
 		addition,
 		subtraction,
 		multiplication,
@@ -17,34 +17,48 @@ namespace rush::ast {
 	};
 
 	class binary_expression : public expression {
-		friend std::unique_ptr<binary_operator> make_binary_expr(
-			std::unique_ptr<expression>,
-			std::unique_ptr<expression>,
-			binary_operator);
+		struct factory_tag_t {};
 
 	public:
-		binary_operator opkind() const noexcept { return _opkind; }
-		expression const& left() const noexcept { return *_left; }
-		expression const& right() const noexcept { return *_right; }
+		binary_expression(
+			std::unique_ptr<expression> left,
+			std::unique_ptr<expression> right,
+			binary_operator op, factory_tag_t)
+			: _left(std::move(left))
+			, _right(std::move(right))
+			, _opkind(op) {}
 
-		virtual void accept(ast::visitor&) const override;
+		expression const& left_operand() const noexcept { return *_left; }
+		expression const& right_operand() const noexcept { return *_right; }
+		binary_operator opkind() const noexcept { return _opkind; }
+
+		virtual ast::type result_type() const override {
+			return ast::intersection_of(
+				_left->result_type(),
+				_right->result_type());
+		}
+
+		virtual void accept(ast::visitor& vis) const override {
+			vis.visit_binary_expr(*this);
+		}
 
 	private:
 		std::unique_ptr<expression> _left;
 		std::unique_ptr<expression> _right;
 		binary_operator _opkind;
 
-		binary_expression(std::unique_ptr<expression> left, std::unique_ptr<expression> right, binary_operator op)
-			: _left(std::move(left))
-			, _right(std::move(right))
-			, _opkind(op) {}
+		friend std::unique_ptr<binary_expression> make_binary_expr(
+			std::unique_ptr<expression> left,
+			std::unique_ptr<expression> right,
+			binary_operator op
+		);
 	};
 
-	std::unique_ptr<binary_expression> addition_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
-	std::unique_ptr<binary_expression> subtraction_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
-	std::unique_ptr<binary_expression> multiplication_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
-	std::unique_ptr<binary_expression> division_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
-	std::unique_ptr<binary_expression> modulo_expr(std::unique_ptr<expression>, std::unique_ptr<expression>);
+	std::unique_ptr<binary_expression> addition_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs);
+	std::unique_ptr<binary_expression> subtraction_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs);
+	std::unique_ptr<binary_expression> multiplication_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs);
+	std::unique_ptr<binary_expression> division_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs);
+	std::unique_ptr<binary_expression> modulo_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs);
 } // rush
 
 #endif // RUSH_AST_BINARY_HPP
---
+++ lib/ast/binary.cpp
@@ -0,0 +1,36 @@
+#include "rush/ast/binary.hpp"
+
+namespace rush::ast {
+	inline std::unique_ptr<binary_expression> make_binary_expr(
+		std::unique_ptr<expression> left,
+		std::unique_ptr<expression> right,
+		binary_operator op
+	) {
+		return std::make_unique<binary_expression>(
+			std::move(left),
+			std::move(right),
+			op, binary_expression::factory_tag_t{}
+		);
+	}
+
+
+	std::unique_ptr<binary_expression> addition_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs) {
+		return make_binary_expr(std::move(lhs), std::move(rhs), binary_operator::addition);
+	}
+
+	std::unique_ptr<binary_expression> subtraction_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs) {
+		return make_binary_expr(std::move(lhs), std::move(rhs), binary_operator::subtraction);
+	}
+
+	std::unique_ptr<binary_expression> multiplication_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs) {
+		return make_binary_expr(std::move(lhs), std::move(rhs), binary_operator::multiplication);
+	}
+
+	std::unique_ptr<binary_expression> division_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs) {
+		return make_binary_expr(std::move(lhs), std::move(rhs), binary_operator::division);
+	}
+
+	std::unique_ptr<binary_expression> modulo_expr(std::unique_ptr<expression> lhs, std::unique_ptr<expression> rhs) {
+		return make_binary_expr(std::move(lhs), std::move(rhs), binary_operator::modulo);
+	}
+} // rush
