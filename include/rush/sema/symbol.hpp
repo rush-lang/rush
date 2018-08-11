a34f945925c45bd29a5c385f3160a37f081220b4
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-11 14:54:03 +0100

    Stub symbol public interface

--- include/rush/sema/symbol.hpp
+++ include/rush/sema/symbol.hpp
@@ -3,8 +3,46 @@
 #ifndef RUSH_SEMA_SYMBOL_HPP
 #define RUSH_SEMA_SYMBOL_HPP
 
+#include <string>
+
 namespace rush {
-	class symbol {};
+	class symbol {
+	public:
+		std::string name() const noexcept {
+			return "";
+		}
+
+		bool is_type() const noexcept {
+			return is_defined()
+				&& (is_function()
+				|| false);
+		}
+
+		bool is_function() const noexcept {
+			return is_defined() && false;
+		}
+
+		bool is_constant() const noexcept {
+			return is_defined() && false;
+		}
+
+		bool is_variable() const noexcept {
+			return is_defined() && false;
+		}
+
+		bool is_builtin() const noexcept {
+			return true;
+		}
+
+		bool is_defined() const noexcept {
+			return false;
+		}
+	};
+
+	symbol make_builtin_symbol(std::string name);
+	symbol make_constant_symbol(std::string name);
+	symbol make_variable_symbol(std::string name);
+	symbol make_function_symbol(std::string name);
 }
 
 #endif // RUSH_SEMA_SYMBOL_HPP
---
+++ test/unit-tests/sema/symbol_test.cpp
@@ -0,0 +1,6 @@
+#include "catch2/catch.hpp"
+#include "rush/sema/symbol.hpp"
+
+TEST_CASE( "rush::symbol", "[unit][sema]" ) {
+
+}
