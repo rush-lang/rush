ee7876dc38d7218c40bbe8407502e6b6570b9ac1
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-27 16:27:48 +0100

    Add help display

--- tools/rush-dump/driver.cpp
+++ tools/rush-dump/driver.cpp
@@ -13,7 +13,7 @@ void dump_lex(std::string src) {
 }
 
 int main(int argc, char const** argv) {
-	auto options = cxxopts::Options { "rush-dump", "Diagnostic tool" };
+	auto options = cxxopts::Options { "rush-dump", "Rush Dump - Diagnostic tool" };
 	options.add_options()
 		("h,help", "Print usage information and exit.")
 		("l,lex", "Display results of the lexical analysis.")
@@ -21,8 +21,14 @@ int main(int argc, char const** argv) {
 		("s,symbols", "Display symbol table information.")
 		("srcs", "Display symbol table information.", cxxopts::value<std::vector<std::string>>())
 	; options.parse_positional({ "srcs" });
+	options.positional_help("\"[ <expression> | <declaration> ]\"");
 
 	auto results = options.parse(argc, argv);
+
+	if (results.count("help") > 0) {
+		std::cout << options.help() << std::endl;
+		return 0;
+	}
 
 	if (results.count("srcs") > 0) {
 		auto srcs = results["srcs"].as<std::vector<std::string>>();
@@ -48,6 +54,9 @@ int main(int argc, char const** argv) {
 
 			++page;
 		}
+	} else {
+		std::cout << "No expression or declaration supplied." << std::endl;
+		std::cout << options.help() << std::endl;
 	}
 
 	return 0;
