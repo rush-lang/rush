0968cc22e139d4b092899b2da3d44ee41a979e12
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-27 16:24:07 +0100

    Replace trailing buffer with new lookahead type

---
+++ include/rush/lexer/lookahead.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+#ifndef RUSH_LEXER_LOOKAHEAD_HPP
+#define RUSH_LEXER_LOOKAHEAD_HPP
+
+#include "rush/core/ring.hpp"
+#include "rush/core/iterator.hpp"
+#include "rush/core/algorithm.hpp"
+#include "rush/core/iterator_ref.hpp"
+
+#include <iosfwd>
+#include <iterator>
+
+namespace rush {
+	template <typename InIter, std::size_t N>
+	class lookahead_buffer {
+	public:
+		using buffer_type = rush::ring<
+			typename std::iterator_traits<InIter>::value_type, N>;
+
+		using value_type = typename buffer_type::value_type;
+		using reference = typename buffer_type::reference;
+		using pointer = typename buffer_type::pointer;
+		using size_type = typename buffer_type::size_type;
+		using difference_type = typename buffer_type::difference_type;
+
+		lookahead_buffer(InIter first, InIter last)
+			: _iter(first)
+			, _last(last) { _initialize(); }
+
+		bool empty() const noexcept {
+			return _finished()
+				&& _buf.empty();
+		}
+
+		size_type size() const noexcept {
+			return _buf.size();
+		}
+
+		size_type capacity() const noexcept {
+			return _buf.capacity();
+		}
+
+		reference peek(difference_type offset = 0) {
+			_check_offset(offset);
+			return *rush::advance__new(
+				_buf.begin(),
+				_buf.end(),
+				offset);
+		}
+
+		void skip() {
+			if (!_finished()) {
+				_buf.push_back(*_iter);
+				++_iter;
+			} else {
+				_check_offset(0);
+				_buf.pop_front();
+			}
+		}
+
+		void skip(difference_type offset) {
+			for (; offset > 0; --offset) skip();
+		}
+
+		value_type next() {
+			auto prev = std::move(_buf.front()); skip();
+			return std::move(prev);
+		}
+
+	private:
+		InIter _iter, _last;
+		buffer_type _buf;
+
+		bool _finished() const noexcept {
+			return _iter == _last;
+		}
+
+		void _check_offset(difference_type offset) const {
+			if (offset >= _buf.size()) throw std::out_of_range("offset out of range");
+		}
+
+		void _initialize() {
+			rush::copy_n(
+				rush::iterator_ref(_iter), _last, N,
+				std::back_inserter(_buf));
+		}
+	};
+
+	template <std::size_t N, typename Container>
+	lookahead_buffer<typename Container::iterator, N> lookahead(Container& cont) {
+		return { begin(cont), end(cont) };
+	}
+
+	template <std::size_t N, typename InIter>
+	lookahead_buffer<InIter, N> lookahead(InIter first, InIter last) {
+		return { first, last };
+	}
+}
+
+#endif // RUSH_LEXER_LOOKAHEAD_HPP
---
+++ test/unit-tests/lexer/lookahead_test.cpp
@@ -0,0 +1,75 @@
+#include "catch2/catch.hpp"
+#include "rush/lexer/lookahead.hpp"
+
+#include <vector>
+
+TEST_CASE( "rush::lookahead_buffer", "[unit][lexer]" ) {
+
+	GIVEN( "A vector of 'n' elements" ) {
+		std::vector<int> v { 1, 2, 3 };
+
+		AND_THEN( "A lookahead_buffer of 'k' < 'n'" ) {
+			auto lab = rush::lookahead<2>(v);
+
+			THEN( "The buffer should not be empty()" ) { REQUIRE_FALSE( lab.empty() ); }
+			THEN( "The buffers size() should be at capacity()" ) { REQUIRE( lab.size() == 2 ); }
+			THEN( "The buffers capacity() should be == 'k'" ) { REQUIRE( lab.capacity() == 2 ); }
+
+			THEN( "peek()'ing at the first element of the buffer" ) { REQUIRE( lab.peek() == 1 ); }
+			THEN( "peek()'ing at the last element of the buffer" ) { REQUIRE( lab.peek(1) == 2 ); }
+			THEN( "peek()'ing past the last element of the buffer" ) { /* should throw */ }
+
+			WHEN( "A single element is consumed with next()" ) {
+				auto prev = lab.next();
+
+				THEN( "The buffer should not be empty()" ) { REQUIRE_FALSE( lab.empty() ); }
+				THEN( "The buffers size() should be at capacity" ) { REQUIRE( lab.size() == 2 ); }
+				THEN( "The buffers capacity() should be == 'k'" ) { REQUIRE( lab.capacity() == 2 ); }
+
+				THEN( "peek()'ing at the first element" ) { REQUIRE( lab.peek() == 2 ); }
+				THEN( "peek()'ing at the last element" ) { REQUIRE( lab.peek(1) == 3 ); }
+				THEN( "The element returned should be the one popped from the front of the buffer" ) { REQUIRE( prev == 1 ); }
+			}
+
+			WHEN( "All elements in the buffer are consumed with skip()" ) {
+				lab.skip(v.size());
+
+				THEN( "The buffer should be empty()" ) { REQUIRE( lab.empty() ); }
+				THEN( "The buffer size() should be == 0" ) { REQUIRE( lab.size() == 0 ); }
+				THEN( "The buffer capacity() should still be == 'k'" ) { REQUIRE( lab.capacity() == 2 ); }
+			}
+		}
+
+		AND_THEN( "A lookahead_buffer of 'k' > 'n'" ) {
+			auto lab = rush::lookahead<4>(v);
+
+			THEN( "The buffer should not be empty()" ) { REQUIRE_FALSE( lab.empty() ); }
+			THEN( "The buffers size() should be == 'n'" ) { REQUIRE( lab.size() == 3 ); }
+			THEN( "The buffers capacity() should be == 'k'" ) { REQUIRE( lab.capacity() == 4 ); }
+
+			THEN( "peek()'ing at the first element of the buffer" ) { REQUIRE( lab.peek() == 1 ); }
+			THEN( "peek()'ing at the last element of the buffer" ) { REQUIRE( lab.peek(2) == 3 ); }
+			THEN( "peek()'ing past the last element of the buffer" ) { /* should throw */ }
+
+			WHEN( "A single element is consumed with next()" ) {
+				auto prev = lab.next();
+
+				THEN( "The buffer should not be empty()" ) { REQUIRE_FALSE( lab.empty() ); }
+				THEN( "The buffers size() should be reduced" ) { REQUIRE( lab.size() == 2 ); }
+				THEN( "The buffers capacity() should be == 'k'" ) { REQUIRE( lab.capacity() == 4 ); }
+
+				THEN( "peek()'ing at the first element" ) { REQUIRE( lab.peek() == 2 ); }
+				THEN( "peek()'ing at the last element" ) { REQUIRE( lab.peek(1) == 3 ); }
+				THEN( "The element returned should be the one popped from the front of the buffer" ) { REQUIRE( prev == 1 ); }
+			}
+
+			WHEN( "All elements in the buffer are consumed with skip()" ) {
+				lab.skip(v.size());
+
+				THEN( "The buffer should be empty()" ) { REQUIRE( lab.empty() ); }
+				THEN( "The buffer size() should be == 0" ) { REQUIRE( lab.size() == 0 ); }
+				THEN( "The buffer capacity() should still be == 'k'" ) { REQUIRE( lab.capacity() == 4 ); }
+			}
+		}
+	}
+}
