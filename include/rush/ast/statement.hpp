313dfb159e2e28aeb733910738670cba34d2729e
Reegan Layzell <reegan.layzell@live.co.uk>
2018-08-30 20:19:17 +0100

    Update statement kinds

--- include/rush/ast/statement.hpp
+++ include/rush/ast/statement.hpp
@@ -6,10 +6,16 @@
 #include "rush/ast/node.hpp"
 #include "rush/sema/scope.hpp"
 
+#include <cstdint>
+
 namespace rush::ast {
-	enum class statement_kind {
+	enum class statement_kind : std::uint8_t {
+		loop
+		branch,
+		control,
+		expression,
+		declaration,
 		block,
-		return_,
 	};
 
 	class statement : public node {
