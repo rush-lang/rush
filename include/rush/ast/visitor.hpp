0277515d37b9b49e0821016d28a72a9fb3078eb2
Reegan Layzell <reegan.layzell@live.co.uk>
2018-09-21 18:02:30 +0100

    Add statements visitation

--- include/rush/ast/visitor.hpp
+++ include/rush/ast/visitor.hpp
@@ -8,13 +8,25 @@ namespace rush::ast {
 	class expression;
 	class unary_expression;
 	class binary_expression;
+	class identifier_expression;
+
 	class literal_expression;
+	class nil_literal_expression;
 	class string_literal_expression;
 	class boolean_literal_expression;
 	class integer_literal_expression;
 	class floating_literal_expression;
-	class identifier_expression;
 
+	class statement;
+	class statement_block;
+	class if_statement;
+	class for_statement;
+	class case_statement;
+	class switch_statement;
+	class while_statement;
+	class return_statement;
+
+	class declaration;
 	class constant_declaration;
 	class variable_declaration;
 
@@ -22,14 +34,29 @@ namespace rush::ast {
 	public:
 		virtual ~visitor() = default;
 
-		virtual void visit_type(ast::type const&) = 0;
-		virtual void visit_unary_expr(unary_expression const&) = 0;
-		virtual void visit_binary_expr(binary_expression const&) = 0;
-		virtual void visit_literal_expr(literal_expression const&) = 0;
-		virtual void visit_identifier_expr(identifier_expression const&) = 0;
+		// types
+		virtual void visit_type(ast::type const&) {};
 
-		virtual void visit_constant_decl(constant_declaration const&) = 0;
-		virtual void visit_variable_decl(variable_declaration const&) = 0;
+		// declarations
+		virtual void visit_constant_decl(constant_declaration const&) {};
+		virtual void visit_variable_decl(variable_declaration const&) {};
+
+		// statements
+		virtual void visit_block_stmt(statement_block const&) {};
+
+		virtual void visit_if_stmt(if_statement const&) {};
+		virtual void visit_switch_stmt(switch_statement const&) {};
+
+		virtual void visit_for_stmt(for_statement const&) {};
+		virtual void visit_while_stmt(while_statement const&) {};
+
+		virtual void visit_return_stmt(return_statement const&) {};
+
+		// expressions
+		virtual void visit_unary_expr(unary_expression const&) {};
+		virtual void visit_binary_expr(binary_expression const&) {};
+		virtual void visit_literal_expr(literal_expression const&) {};
+		virtual void visit_identifier_expr(identifier_expression const&) {};
 
 		virtual void visit_literal_expr(string_literal_expression const&) {}
 		virtual void visit_literal_expr(boolean_literal_expression const&) {}
