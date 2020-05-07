dd87ce19ec051f4e675bc56aa2196043d62a4a0b
Reegan Layzell <reegan.layzell@live.co.uk>
2020-05-07 01:25:17 +0100

    Add lambda expressions to invoke result type resolver

--- lib/ast/exprs/invoke.cpp
+++ lib/ast/exprs/invoke.cpp
@@ -1,6 +1,5 @@
 #include "rush/ast/exprs/invoke.hpp"
 #include "rush/ast/types/type.hpp"
-#include "rush/ast/types/builtin.hpp"
 #include "rush/ast/visitor.hpp"
 #include "rush/ast/expressions.hpp"
 #include "rush/ast/declarations.hpp"
@@ -16,10 +15,8 @@ public:
       return _result;
    }
 
-   virtual void visit_function_type(ast::function_type const& type) override {
-      // ultimately we're trying to find this.
-      _result = type.return_type();
-   }
+   // ultimately we're trying to find this.
+   virtual void visit_function_type(ast::function_type const& type) override { _result = type.return_type(); }
 
    virtual void visit_constant_decl(ast::constant_declaration const& decl) override { decl.type().accept(*this); }
    virtual void visit_variable_decl(ast::variable_declaration const& decl) override { decl.type().accept(*this); }
@@ -27,27 +24,25 @@ public:
 
    virtual void visit_unary_expr(ast::unary_expression const& expr) override { expr.result_type().accept(*this); }
    virtual void visit_binary_expr(ast::binary_expression const& expr) override { expr.result_type().accept(*this); }
-   virtual void visit_invoke_expr(ast::invoke_expression const& expr) override { expr.result_type().accept(*this); }
    virtual void visit_ternary_expr(ast::ternary_expression const& expr) override { expr.result_type().accept(*this); }
+   virtual void visit_lambda_expr(ast::lambda_expression const& expr) override { expr.result_type().accept(*this); }
+   virtual void visit_invoke_expr(ast::invoke_expression const& expr) override { expr.result_type().accept(*this); }
 
    virtual void visit_identifier_expr(ast::identifier_expression const& expr) override {
-      if (!expr.is_unresolved()) { expr.declaration().accept(*this); return; }
-      _result = undeclared_identifier_error_type;
+      _result = !expr.is_unresolved()
+              ? rush::visit(expr.declaration(), *this).result()
+              : ast::types::undeclared;
    }
 
    virtual void visit_member_access_expr(ast::member_access_expression const& expr) override {
-      if (!expr.is_unresolved()) { expr.result_type().accept(*this); return; }
-      _result = undeclared_identifier_error_type;
+      _result = !expr.is_unresolved()
+              ? rush::visit(expr.result_type(), *this).result()
+              : ast::types::undeclared;
    }
 
 private:
-   static const ast::builtin_error_type undeclared_identifier_error_type;
    ast::type_ref _result;
 };
-
-const ast::builtin_error_type invoke_result_type_resolver::
-   undeclared_identifier_error_type =
-   ast::types::error_type("undeclared identifier.");
 
 namespace rush::ast {
    ast::type_ref invoke_expression::result_type() const {
